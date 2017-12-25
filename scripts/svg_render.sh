7cea6223ba4b0fc16e340604568e6e1920f4ae77
Matt Demanett <matt@demanett.net>
2017-12-25 14:03:22 -0600

    Simplify script args.

--- scripts/svg_render.sh
+++ scripts/svg_render.sh
@@ -2,15 +2,24 @@
 
 INKSCAPE="/Applications/Inkscape.app/Contents/Resources/bin/inkscape"
 
-if [ "x$1" = "x" -o "x$2" = "x" ]
+if [ "x$1" = "x" ]
 then
-  echo "Usage: $0 <in.svg> <out.svg>"
+  echo "Usage: $0 <IN-src.svg>"
   exit 1
 fi
 
 if [ ! -f "$1" ]
 then
   echo "No such file: $1"
+  exit 1
+fi
+
+OUT_FILE=''
+if echo "$1" | perl -e '$_ = <stdin>; exit(!/-src\.svg$/i)'
+then
+  OUT_FILE=`echo "$1" | perl -e '$_ = <stdin>; s/-src\.svg$/.svg/i; print'`
+else
+  echo "Input file must be named *-src.svg: $1"
   exit 1
 fi
 
@@ -22,5 +31,5 @@ TMP_FILE="/tmp/svg_render_tmp.svg"
   --verb EditSelectAll --verb EditUnlinkClone \
   --verb EditSelectAll  --verb ObjectToPath \
   --verb FileSave --verb FileQuit && \
-  cp "$TMP_FILE" "$2" && \
+  cp "$TMP_FILE" "$OUT_FILE" && \
   rm "$TMP_FILE"
