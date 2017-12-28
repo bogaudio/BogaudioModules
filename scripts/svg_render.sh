e1843bccd6eaa3f18ea8a6eae3f9a85b45114610
Matt Demanett <matt@demanett.net>
2017-12-27 21:41:35 -0600

    Segregate source and rendered svgs by directory.

--- scripts/svg_render.sh
+++ scripts/svg_render.sh
@@ -17,7 +17,7 @@ fi
 OUT_FILE=''
 if echo "$1" | perl -e '$_ = <stdin>; exit(!/-src\.svg$/i)'
 then
-  OUT_FILE=`echo "$1" | perl -e '$_ = <stdin>; s/-src\.svg$/.svg/i; print'`
+  OUT_FILE=`echo "$1" | perl -e '$_ = <stdin>; s/-src\.svg$/.svg/i; s/res-src\//res\//; print'`
 else
   echo "Input file must be named *-src.svg: $1"
   exit 1
