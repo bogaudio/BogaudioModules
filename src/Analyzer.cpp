c96c5b7e1f2645b614b261913936cd22fd45e3e7
Matt Demanett <matt@demanett.net>
2018-01-02 22:40:14 -0500

    Min Analyzer graph line width.

--- src/Analyzer.cpp
+++ src/Analyzer.cpp
@@ -282,7 +282,7 @@ void AnalyzerDisplay::draw(NVGcontext* vg) {
 	drawBackground(vg);
 
 	if (_module->_channelA || _module->_channelB || _module->_channelC || _module->_channelD) {
-		float strokeWidth = 3 - gRackScene->zoomWidget->zoom;
+		float strokeWidth = std::max(1.0f, 3 - gRackScene->zoomWidget->zoom);
 		nvgSave(vg);
 		nvgScissor(vg, _insetAround, _insetAround, _size.x - _insetAround, _size.y - _insetAround);
 		drawHeader(vg);
