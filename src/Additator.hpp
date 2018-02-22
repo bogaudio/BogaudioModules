69c0033c128595e60d5cd81206ce9d41ebec2183
Matt Demanett <matt@demanett.net>
2018-02-22 01:14:56 -0500

    Additator: more work.

--- src/Additator.hpp
+++ src/Additator.hpp
@@ -15,10 +15,12 @@ struct Additator : Module {
 		FREQUENCY_PARAM,
 		PARTIALS_PARAM,
 		WIDTH_PARAM,
-		EVEN_WIDTH_PARAM,
+		ODD_SKEW_PARAM,
+		EVEN_SKEW_PARAM,
+		GAIN_PARAM,
 		DECAY_PARAM,
-		ODD_SCALE_PARAM,
-		EVEN_SCALE_PARAM,
+		BALANCE_PARAM,
+		FILTER_PARAM,
 		PHASE_PARAM,
 		NUM_PARAMS
 	};
@@ -26,6 +28,14 @@ struct Additator : Module {
 	enum InputsIds {
 		PITCH_INPUT,
 		SYNC_INPUT,
+		PARTIALS_INPUT,
+		WIDTH_INPUT,
+		ODD_SKEW_INPUT,
+		EVEN_SKEW_INPUT,
+		GAIN_INPUT,
+		DECAY_INPUT,
+		BALANCE_INPUT,
+		FILTER_INPUT,
 		NUM_INPUTS
 	};
 
@@ -35,17 +45,36 @@ struct Additator : Module {
 	};
 
 	enum LightsIds {
+		SINE_LIGHT,
+		COSINE_LIGHT,
 		NUM_LIGHTS
 	};
 
+	enum Phase {
+		PHASE_RESET,
+		PHASE_SINE,
+		PHASE_COSINE
+	};
+
 	const int maxPartials = 100;
+	const float maxWidth = 2.0f;
+	const float maxSkew = 0.99f;
+	const float minAmplitudeNormalization = 1.0f;
+	const float maxAmplitudeNormalization = 5.0f;
+	const float minDecay = -1.0f;
+	const float maxDecay = 3.0f;
+	const float minFilter = 0.1;
+	const float maxFilter = 1.9;
+
 	int _partials = 0;
 	float _width = 0.0f;
-	float _evenWidth = 0.0f;
+	float _oddSkew = 0.0f;
+	float _evenSkew = 0.0f;
+	float _amplitudeNormalization = 0.0f;
 	float _decay = 0.0f;
-	float _oddScale = 0.0f;
-	float _evenScale = 0.0f;
-	float _phase = 0.0f;
+	float _balance = 0.0f;
+	float _filter = 0.0f;
+	Phase _phase = PHASE_RESET;
 	SineBankOscillator _oscillator;
 	SchmittTrigger _syncTrigger;
 
@@ -58,6 +87,7 @@ struct Additator : Module {
 
 	virtual void onReset() override;
 	virtual void onSampleRateChange() override;
+	float cvValue(Input& cv);
 	virtual void step() override;
 };
 
