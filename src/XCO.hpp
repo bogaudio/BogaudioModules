8828129f2e4802e8c2ad7eea817193e629f71c2e
Matt Demanett <matt@demanett.net>
2018-03-21 02:14:02 -0400

    XCO implementation.

--- src/XCO.hpp
+++ src/XCO.hpp
@@ -1,6 +1,9 @@
 #pragma once
 
 #include "bogaudio.hpp"
+#include "dsp/oscillator.hpp"
+
+using namespace bogaudio::dsp;
 
 extern Model* modelXCO;
 
@@ -13,7 +16,7 @@ struct XCO : Module {
 		SLOW_PARAM,
 		FM_PARAM,
 		FM_TYPE_PARAM,
-		SQUARE_PWM_PARAM,
+		SQUARE_PW_PARAM,
 		SQUARE_PHASE_PARAM,
 		SQUARE_MIX_PARAM,
 		SAW_SATURATION_PARAM,
@@ -28,7 +31,7 @@ struct XCO : Module {
 
 	enum InputsIds {
 		FM_INPUT,
-		SQUARE_PWM_INPUT,
+		SQUARE_PW_INPUT,
 		SQUARE_PHASE_INPUT,
 		SQUARE_MIX_INPUT,
 		SAW_SATURATION_INPUT,
@@ -59,12 +62,48 @@ struct XCO : Module {
 		NUM_LIGHTS
 	};
 
-	XCO() : Module(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS) {
+	const int modulationSteps = 100;
+	const float amplitude = 5.0f;
+	int _modulationStep = 0;
+	float _baseVOct = 0.0f;
+	float _baseHz = 0.0f;
+	bool _slowMode = false;
+	float _fmDepth = 0.0f;
+	bool _fmLinearMode = false;
+	float _squarePhaseOffset = 0.0f;
+	float _sawPhaseOffset = 0.0f;
+	float _trianglePhaseOffset = 0.0f;
+	float _sinePhaseOffset = 0.0f;
+	float _squareMix = 1.0f;
+	float _sawMix = 1.0f;
+	float _triangleMix = 1.0f;
+	float _sineMix = 1.0f;
+	SchmittTrigger _syncTrigger;
+
+	Phasor _phasor;
+	BandLimitedSquareOscillator _square;
+	BandLimitedSawOscillator _saw;
+	TriangleOscillator _triangle;
+	SineTableOscillator _sine;
+
+	XCO()
+	: Module(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS)
+	, _phasor(engineGetSampleRate(), 1.0f)
+	, _square(0.0f, 0.0f)
+	, _saw(0.0f, 0.0f)
+	, _triangle(0.0f, 0.0f)
+	, _sine(0.0f, 0.0f)
+	{
 		onReset();
+		_saw.setQuality(12);
+		_square.setQuality(12);
 	}
 
 	virtual void onReset() override;
+	virtual void onSampleRateChange() override;
 	virtual void step() override;
+	float phaseOffset(Param& param, Input& input);
+	float level(Param& param, Input& input);
 };
 
 } // namespace bogaudio
