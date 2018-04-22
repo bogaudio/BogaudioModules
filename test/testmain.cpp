95b491117c96576a24150ef5463486f2368dd784
Matt Demanett <matt@demanett.net>
2018-04-21 20:20:34 -0400

    Rudimentary fixed-width math.

--- test/testmain.cpp
+++ test/testmain.cpp
@@ -8,17 +8,52 @@
 
 #include "util/math.hpp" // Rack
 
+#include "dsp/fixed.hpp"
+
+using namespace bogaudio::dsp;
+
 int main() {
-  // rando math or whatever, test it here!
+	{
+		fixed_16_16 x = 5;
+		fixed_16_16 y = 1;
+		x = 3;
+		printf("X=%d\n", (int)(x + y));
+		y = 2;
+		printf("X=%d\n", (int)(x - y));
+		x = y + 5;
+		printf("X=%d\n", (int)x);
+		x = y - 3;
+		printf("X=%d\n", (int)x);
 
-  printf("%f\n", rack::eucmod(M_PI, 2.0f));
-  printf("%f\n", rack::eucmod(-M_PI, 2.0f));
-  printf("%f\n", rack::eucmod(M_PI, -2.0f));
-  printf("%f\n", rack::eucmod(-M_PI, -2.0f));
-  printf("%f\n", fmodf(M_PI, 2.0f));
-  printf("%f\n", fmodf(-M_PI, 2.0f));
-  printf("%f\n", fmodf(M_PI, -2.0f));
-  printf("%f\n", fmodf(-M_PI, -2.0f));
+		x += 2.5;
+		printf("X=%d\n", (int)x);
+		printf("X=%f\n", (float)x);
 
-  return 0;
+		x = y - 0.3;
+		printf("X=%d\n", (int)x);
+		printf("X=%f\n", (float)x);
+	}
+
+	{
+		fixed_32_32 x = 5;
+		fixed_32_32 y = 1;
+		x = 3;
+		printf("X=%d\n", (int)(x + y));
+		y = 2;
+		printf("X=%d\n", (int)(x - y));
+		x = y + 5;
+		printf("X=%d\n", (int)x);
+		x = y - 3;
+		printf("X=%d\n", (int)x);
+
+		x += 2.5;
+		printf("X=%d\n", (int)x);
+		printf("X=%f\n", (float)x);
+
+		x = y - 0.3;
+		printf("X=%d\n", (int)x);
+		printf("X=%f\n", (float)x);
+	}
+
+	return 0;
 }
