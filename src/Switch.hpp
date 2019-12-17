4b37577c4a451c41c5784b2d6c2b1ba3368bd94a
Matt Demanett <matt@demanett.net>
2019-12-17 00:13:27 -0500

    SWITCH: add lights to indicate state. #95

--- src/Switch.hpp
+++ src/Switch.hpp
@@ -28,23 +28,36 @@ struct Switch : BGModule {
 		NUM_OUTPUTS
 	};
 
+	enum LightsIds {
+		HIGH1_LIGHT,
+		LOW1_LIGHT,
+		HIGH2_LIGHT,
+		LOW2_LIGHT,
+		NUM_LIGHTS
+	};
+
 	Trigger _trigger[maxChannels];
 	bool _latchedHigh[maxChannels] {};
 	bool _latch = false;
+	int _high1LightSum = 0;
+	int _low1LightSum = 0;
+	int _high2LightSum = 0;
+	int _low2LightSum = 0;
 
 	Switch() {
-		config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS);
+		config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
 		configParam(GATE_PARAM, 0.0f, 10.0f, 0.0f, "Gate");
 		configParam(LATCH_PARAM, 0.0f, 1.0f, 0.0f, "Latch");
 		reset();
 	}
 
 	void reset() override;
-	bool active() override;
 	int channels() override;
 	void channelsChanged(int before, int after) override;
 	void modulate() override;
+	void processAll(const ProcessArgs& args) override;
 	void processChannel(const ProcessArgs& args, int _c) override;
+	void postProcess(const ProcessArgs& args) override;
 };
 
 } // namespace bogaudio
