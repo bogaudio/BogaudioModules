a0058d71a71a72972779848691d6ec07f4195833
Matt Demanett <matt@demanett.net>
2020-08-02 21:48:35 -0400

    Fix svg_render.rb to process multiple files in one call, and with one invocation of Inkscape.

--- scripts/svg_render.rb
+++ scripts/svg_render.rb
@@ -1,29 +1,8 @@
 #!/usr/bin/ruby
 
 INKSCAPE = '/Applications/Inkscape.app/Contents/MacOS/inkscape'
-TMP_FILE = '/tmp/svg_render_tmp.svg'
 
 require 'nokogiri'
-
-if ARGV.empty?
-  STDERR.puts "Usage: #{$0} <res-pp/MODULE-pp.svg>"
-  exit 1
-end
-
-svg_file = ARGV[0]
-unless File.readable?(svg_file)
-  STDERR.puts "No such file: #{svg_file}"
-  exit 1
-end
-
-name = nil
-if File.basename(svg_file) =~ /^(.*)-pp.svg$/
-  name = $1
-else
-  STDERR.puts "Input file must be named *-pp.svg"
-  exit 1
-end
-dir = File.dirname(svg_file)
 
 def read_xml(fn)
   Nokogiri::XML(File.open(fn)) do |config|
@@ -31,27 +10,62 @@ def read_xml(fn)
   end
 end
 
-Dir.glob(File.join(File.dirname(svg_file), "#{name}*-pp.svg")).each do |fn|
-  puts "Processing #{fn}..."
-  doc = read_xml(fn)
+if ARGV.empty?
+  STDERR.puts "Usage: #{$0} <res-pp/MODULE-pp.svg> [...more files...]"
+  exit 1
+end
 
-  doc.css('use').each do |n|
-    id = n.attribute('id')
-    n.remove if id && id.to_s =~ /_(PARAM|INPUT|OUTPUT|LIGHT|WIDGET)$/
-  end
-  File.write(TMP_FILE, doc.to_xml)
-
-  out = `#{INKSCAPE} -g --actions='EditSelectAll;SelectionUnGroup;EditSelectAll;EditUnlinkClone;EditSelectAll;ObjectToPath;FileSave;FileQuit' '#{TMP_FILE}' 2>&1`
-  unless $?.success?
-    STDERR.puts "Calling Inkscape failed:\n#{out}"
+svg_filenames = {}
+svg_files = []
+ARGV.each do |file|
+  unless File.readable?(file)
+    STDERR.puts "No such file: #{file}"
     exit 1
   end
 
-  doc = read_xml(TMP_FILE)
-  doc.css('style').each(&:remove)
+  name = nil
+  if File.basename(file) =~ /^(.*)-pp.svg$/
+    name = $1
+  else
+    STDERR.puts "Input file #{file} must be named *-pp.svg"
+    exit 1
+  end
 
-  out_file = File.join(dir.sub(/-pp\/?$/, ''), File.basename(fn).sub(/-pp.svg$/, '.svg'))
-  File.write(out_file, doc.to_xml)
-  File.unlink(TMP_FILE)
-  puts "Wrote #{out_file}"
+  Dir.glob(File.join(File.dirname(file), "#{name}*-pp.svg")).each do |fn|
+    unless svg_filenames.key?(fn)
+      svg_filenames[fn] = true
+
+      puts "Preparing #{fn}..."
+      doc = read_xml(fn)
+      doc.css('use').each do |n|
+        id = n.attribute('id')
+        n.remove if id && id.to_s =~ /_(PARAM|INPUT|OUTPUT|LIGHT|WIDGET)$/
+      end
+      tmp_name = File.join('/tmp', "svg_render_#{File.basename(fn)}")
+      File.write(tmp_name, doc.to_xml)
+
+      svg_files << {
+        name: fn,
+        out_name: File.join(File.dirname(fn).sub(/-pp\/?$/, ''), File.basename(fn).sub(/-pp.svg$/, '.svg')),
+        tmp_name: tmp_name
+      }
+    end
+  end
+end
+
+puts 'Calling Inkscape...'
+files = svg_files.map { |f| "'#{f[:tmp_name]}'" }.join(' ')
+out = `#{INKSCAPE} -g --batch-process --actions='EditSelectAll;SelectionUnGroup;EditSelectAll;EditUnlinkClone;EditSelectAll;ObjectToPath;FileSave' #{files} 2>&1`
+unless $?.success?
+  STDERR.puts "Calling Inkscape failed:\n#{out}"
+  exit 1
+end
+
+puts 'Cleaning up...'
+svg_files.each do |sf|
+  fn = sf[:tmp_name]
+  doc = read_xml(fn)
+  doc.css('style').each(&:remove)
+  File.write(sf[:out_name], doc.to_xml)
+  File.unlink(fn)
 end
