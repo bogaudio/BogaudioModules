dabcf1f0d83c8f323191a2f341930173cec64510
Matt Demanett <matt@demanett.net>
2021-11-28 14:23:17 -0500

    Try to get on-push compilation check github action working with rack2.

--- .github/actions/build_osx/entrypoint.sh
+++ .github/actions/build_osx/entrypoint.sh
@@ -9,7 +9,7 @@ export CC=x86_64-apple-darwin15-clang
 export CXX=x86_64-apple-darwin15-clang++
 export STRIP=x86_64-apple-darwin15-strip
 
-curl -L https://vcvrack.com/downloads/Rack-SDK-${RACK_SDK_VERSION}.zip -o rack-sdk.zip
+curl -L https://vcvrack.com/downloads/Rack-SDK-${RACK_SDK_VERSION}-mac.zip -o rack-sdk.zip
 unzip -o rack-sdk.zip
 rm rack-sdk.zip
 
--- .github/actions/build_win/entrypoint.sh
+++ .github/actions/build_win/entrypoint.sh
@@ -9,7 +9,7 @@ export CC=x86_64-w64-mingw32-gcc-posix
 export CXX=x86_64-w64-mingw32-g++-posix
 export STRIP=x86_64-w64-mingw32-strip
 
-curl -L https://vcvrack.com/downloads/Rack-SDK-${RACK_SDK_VERSION}.zip -o rack-sdk.zip
+curl -L https://vcvrack.com/downloads/Rack-SDK-${RACK_SDK_VERSION}-win.zip -o rack-sdk.zip
 unzip -o rack-sdk.zip
 rm rack-sdk.zip
 
