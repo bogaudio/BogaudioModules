43ec641f91453208953724becc13e5d38279b951
Matt Demanett <matt@demanett.net>
2021-12-19 16:23:50 -0500

    Fix autobuild upload script for new v2 plugin file extension.

--- .github/actions/upload_zip/script.sh
+++ .github/actions/upload_zip/script.sh
@@ -14,11 +14,11 @@ curl -o release.json \
 
 UPLOAD_URL=$(jq -r .upload_url release.json)
 
-ASSET_PATH=$(ls dist/*.zip)
+ASSET_PATH=$(ls dist/*.vcvplugin)
 
 curl -i \
     --header "Authorization: token ${GITHUB_TOKEN}" \
-    --header "Content-Type: application/zip" \
+    --header "Content-Type: application/zstd" \
     --request POST \
     --data-binary @"${ASSET_PATH}" \
     ${UPLOAD_URL%"{?name,label\}"}?name=${ASSET_PATH#"dist/"}
