badf131d9dc0a308786d79d3d0a67c94e96d6037
Matt Demanett <matt@demanett.net>
2023-11-27 20:12:05 -0500

    Fix SDK download after version bump.

--- .github/actions/build_linux/entrypoint.sh
+++ .github/actions/build_linux/entrypoint.sh
@@ -5,7 +5,7 @@ set -eu
 export RACK_DIR=${GITHUB_WORKSPACE}/Rack-SDK
 export RACK_USER_DIR=${GITHUB_WORKSPACE}
 
-curl -L https://vcvrack.com/downloads/Rack-SDK-${RACK_SDK_VERSION}-lin.zip -o rack-sdk.zip
+curl -L https://vcvrack.com/downloads/Rack-SDK-${RACK_SDK_VERSION}-lin-x64.zip -o rack-sdk.zip
 unzip -o rack-sdk.zip
 rm rack-sdk.zip
 
